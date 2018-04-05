1c7b294f4b773c2023e3b27639a9a789eb2c6b55
frankiebaffa@gmail.com <frankiebaffa@gmail.com>
2018-04-05 14:14:05 -0400

    add files

---
+++ EntityCustomerCar/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EntityCustomerCar
+{
+    public class Car
+    {
+        public Guid Id { get; set; }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public Guid OwnerId { get; set; }
+        
+        public ICollection<Customer> Customers { get; set; }
+    }
+}
---
+++ EntityCustomerCar/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EntityCustomerCar
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long Phone { get; set; }
+        public DateTime DateAdded { get; set; }
+
+        public Car Car { get; set; }
+
+    }
+}
